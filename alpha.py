6bfe530a8664b3e6fc8d8ac1c90d1463daa902f2
Giovanni Vigna <giovanni.vigna@gmail.com>
2025-09-18 08:43:13 -0700

    First alpha version

--- alpha.py
+++ alpha.py
@@ -31,3 +31,39 @@ if not os.getenv("OPENAI_API_KEY"):
     sys.exit(1)
 
 llm = init_chat_model("openai:gpt-4.1")
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
